d612170e71036f073877bb52e91fd297c821b5a4
Diogo Gonçalves <dgoncalves@agap2.pt>
2022-08-12 15:02:12 +0100

    @BaseActivity: Added logic to create viewmodel and viewBinding

--- src/main/java/com/sqool/droidcore/ui/BaseActivity.kt
+++ src/main/java/com/sqool/droidcore/ui/BaseActivity.kt
@@ -1,7 +1,27 @@
 package com.sqool.droidcore.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
 
-abstract class BaseActivity<T> : AppCompatActivity() where T : BaseViewModel {
+abstract class BaseActivity<B : ViewDataBinding, T : BaseViewModel>(
+    private val layoutId: Int,
+    private val viewModelVariableId: Int,
+    private val viewModelClass: Class<T>
+) :
+    AppCompatActivity() {
 
+    protected lateinit var binding: B
+    protected lateinit var viewModel: T
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        viewModel = ViewModelProvider(this).get(viewModelClass)
+        binding.setVariable(viewModelVariableId, viewModel)
+    }
 }
