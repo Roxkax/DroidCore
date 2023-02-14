bde495e764b5f1842e42bcc03048f938704c0fef
Diogo Gonçalves <dgoncalves@agap2.pt>
2023-02-14 12:36:49 +0000

    @BaseActivty: Removed all the base boilerplate for now

--- src/main/java/com/sqool/droidcore/ui/BaseActivity.kt
+++ src/main/java/com/sqool/droidcore/ui/BaseActivity.kt
@@ -2,26 +2,14 @@ package com.sqool.droidcore.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.ViewModelProvider
 
-abstract class BaseActivity<B : ViewDataBinding, T : BaseViewModel>(
-    private val layoutId: Int,
-    private val viewModelVariableId: Int,
-    private val viewModelClass: Class<T>
-) :
-    AppCompatActivity() {
+abstract class BaseActivity(private val layoutId: Int?) : AppCompatActivity() {
 
-    protected lateinit var binding: B
-    protected lateinit var viewModel: T
-
-
+    constructor() : this(null)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, layoutId)
-        viewModel = ViewModelProvider(this).get(viewModelClass)
-        binding.setVariable(viewModelVariableId, viewModel)
+        layoutId?.let {
+            setContentView(layoutId)
+        }
     }
 }
